6419cb86b573297edace6230297f73c32c8051b6
FrenkyDema <18606@studenti.marconiverona.edu.it>
2023-02-06 17:54:58 +0100

    Update Database
    populate registry table
    populate domicile table
    populate contract table
    Update python insert script

--- Database/insert_src/insert.py
+++ Database/insert_src/insert.py
@@ -6,37 +6,33 @@ from mysql.connector import Error
 path, tail = os.path.split(__file__)
 os.chdir(path)
 
-def find_empty_keys(dict_list):
+
+def find_empty_keys(dict_list: list[dict]):
     empty_keys = set()
-    for i, d in enumerate(dict_list):
+    for ignore, d in enumerate(dict_list):
         for key, value in d.items():
             if not value:
                 empty_keys.add(key)
     return empty_keys
 
-def remove_IDAnagrafica(dict_list):
-    for d in dict_list:
-        if "IDAnagrafica" in d:
-            del d["IDAnagrafica"]
 
-boold = True
-if __name__ == "__main__":
-    if boold:
-        print("Start")
-
-    file_directory = "./file/"
+def remove_extra_args(dict_list: list[dict], args: list[str]):
+    for dictionary in dict_list:
+        for arg in args:
+            if arg in dictionary:
+                del dictionary[arg]
+    return dict_list
 
 
-    csv_filename = 'Anagrafiche.csv'
-
-    with open(file_directory +  csv_filename, encoding='utf-8-sig') as f:
-        reader: list[dict] = remove_IDAnagrafica(list(csv.DictReader(f, delimiter=";")))
-
-        print(reader)
-
-
+def replace_with_null(dict_list: list[dict]):
+    for i, dictionary in enumerate(dict_list):
+        for key, value in dictionary.items():
+            if not value:
+                dict_list[i][key] = None
+    return dict_list
 
 
+def execute_query(dict_list: list[dict], table: str):
     try:
         connection = mysql.connector.connect(host='localhost',
                                              user='root',
@@ -50,17 +46,16 @@ if __name__ == "__main__":
             record = cursor.fetchone()
             print("You're connected to database: ", record)
 
-            table = "registry"
             # query
-            for row in reader:
+            for dictionary in dict_list:
 
-                placeholders = ', '.join(['%s'] * len(row))
-                columns = ', '.join(row.keys())
-                sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (table, columns, placeholders)
+                placeholders = ', '.join(['%s'] * len(dictionary))
+                columns = ', '.join(dictionary.keys())
+                sql = "INSERT INTO %s ( %s ) VALUES ( %s )" % (
+                    table, columns, placeholders)
                 print(sql)
-                cursor.execute(sql, list(row.values()))
-
-
+                cursor.execute(sql, list(dictionary.values()))
+                connection.commit()
 
             # close connection
             cursor.close()
@@ -68,6 +63,47 @@ if __name__ == "__main__":
             print("MySQL connection is closed")
     except Error as e:
         print("Error while connecting to MySQL", e)
+
+
+def open_file_and_execute_query(filename: str, exclude_args: list[str], table: str):
+
+    file_directory = "./file/"
+    with open(file_directory + filename, encoding='utf-8-sig') as f:
+
+        execute_query(
+            replace_with_null(
+                remove_extra_args(
+                    list(csv.DictReader(f, delimiter=";")),
+                    exclude_args
+                )),
+            table
+        )
+
+
+boold = True
+if __name__ == "__main__":
+    if boold:
+        print("Start")
+
+    open_file_and_execute_query(
+        'Anagrafiche.csv',
+        ["IDAnagrafica", "Email"],
+        "registry"
+    )
+
+    open_file_and_execute_query(
+        'Contratti.csv',
+        ["IDRigaContratto"],
+        "contract"
+    )
+
+    open_file_and_execute_query(
+        'Sedi.csv',
+        ["IDSede","IDAnagrafica"],
+        "domicile"
+    )
+
+
 
     if boold:
         print("End")
