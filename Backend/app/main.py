c09c48c901ffa26e93779db598a361bcee13763e
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2023-02-07 12:50:52 +0100

    Update backend
    python api

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -9,12 +9,25 @@ app = FastAPI()
 class User(BaseModel):
     email: str
     pww: str
+    
+class Registry(BaseModel):
+    name: str
+    surname: str
+    
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
+@app.post("/login/")
+def login(user: User):
+    
+    
+    
+    
+    return user 
 
 @app.post("/login/")
 def login(user: User):
