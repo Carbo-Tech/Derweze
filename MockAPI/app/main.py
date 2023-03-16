27de362065fab796accd63e77fde7cd1a90c5557
PedioZaki-3BI-2020-2021 <73580334+PedioZaki-3BI-2020-2021@users.noreply.github.com>
2023-03-16 13:37:40 +0100

    updated mockapi

--- MockAPI/app/main.py
+++ MockAPI/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from typing import List
+import random
 
 app = FastAPI()
 
@@ -6,3 +8,43 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "mockapi"}
+
+@app.get("/getElectricityPlans")
+def get_plans(address: str) -> List[dict]:
+    random.seed(address)
+    plans = [
+        {
+            "id": 1,
+            "company": "Green Energy Co.",
+            "image": "https://example.com/green-energy.jpg",
+            "basePrice": round(random.gauss(100, 100), 2),
+            "pricePerKW": round(random.gauss(10, 5), 2),
+            "priceOscillation": round(random.gauss(5, 5), 2),
+            "estimatedCo2Emittions": round(random.gauss(100, 100), 2),
+            "estimatedGreenEnergy": round(random.gauss(25, 10), 2),
+            "description": "Our standard green energy plan"
+        },
+        {
+            "id": 2,
+            "company": "Cheap Energy Co.",
+            "image": "https://example.com/cheap-energy.jpg",
+            "basePrice": round(random.gauss(100, 100), 2),
+            "pricePerKW": round(random.gauss(10, 5), 2),
+            "priceOscillation": round(random.gauss(5, 5), 2),
+            "estimatedCo2Emittions": round(random.gauss(100, 100), 2),
+            "estimatedGreenEnergy": round(random.gauss(25, 10), 2),
+            "description": "Our cheapest plan"
+        },
+        {
+            "id": 3,
+            "company": "Eco-Friendly Power",
+            "image": "https://example.com/eco-friendly-power.jpg",
+            "basePrice": round(random.gauss(100, 100), 2),
+            "pricePerKW": round(random.gauss(10, 5), 2),
+            "priceOscillation": round(random.gauss(5, 5), 2),
+            "estimatedCo2Emittions": round(random.gauss(100, 100), 2),
+            "estimatedGreenEnergy": round(random.gauss(25, 10), 2),
+            "description": "Our eco-friendly power plan"
+        }
+    ]
+    return {"plans": plans}
